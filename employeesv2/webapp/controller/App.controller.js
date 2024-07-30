b493b602e245175b6f2eb1870af390804af6a6ac
AdalRB <adalramirez@alfasolucionesti.com>
2024-07-30 18:13:42 +0000

    Proyecto final para deploy

---
+++ employeesv2/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+  'sap/ui/core/mvc/Controller',
+], function(Controller) {
+  return Controller.extend("alfa01.employeesv2.controller.App",{
+      onInit : function(){
+
+      }
+  });  
+});
+
+  
---
+++ employeesv2/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"alfa01/employeesv2/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
